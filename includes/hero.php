a2ba59b509925d3baca4bea2fadb938a6b192d6e
dmitrylean <adam.lean.92@gmail.com>
2025-10-26 17:50:01 +0800

    update includes

--- includes/hero.php
+++ includes/hero.php
@@ -1,16 +1,21 @@
-  <section class="hero">
-    <div class="hero-content">
-      <p class="category">Hair Salon, Masseuse, Beauty Salon</p>
-      <h1>Find a service<br>close to you</h1>
-      <p class="desc">There are many variation of passages of Lorem Ipsum available, majority have suffered alteration in some form.</p>
+<?php
+function Hero(string $title, string $subtitle): string {
+    return <<<HTML
+<section class="hero">
+  <div class="hero-content">
+    <p class="category">Hair Salon, Masseuse, Beauty Salon</p>
+    <h1>$title</h1>
+    <p class="desc">$subtitle</p>
 
-      <form class="search-box">
-        <input type="text" placeholder="Book your services...">
-        <input type="text" placeholder="Where">
-        <button type="submit">Search</button>
-      </form>
-    </div>
-  </section>
+    <form class="search-box">
+      <input type="text" placeholder="Book your services...">
+      <input type="text" placeholder="Where">
+      <button type="submit">Search</button>
+    </form>
+  </div>
+</section>
+HTML;
+} ?>
 
   <style>
     /* ---------- HERO SECTION ---------- */
--- pages/home.php
+++ pages/home.php
@@ -1,11 +1,29 @@
 <?php
-  include 'includes/hero.php'; ?>
+require_once __DIR__ . '/../includes/hero.php';
+
+echo Hero(
+    'Find a service<br>close to you',
+    'There are many variations of passages of Lorem Ipsum available.'
+);
+?>
 
 <?php
-  include 'includes/services.php'; ?>
-  
-  <?php
-  include 'includes/testimonials.php'; ?>
+require_once __DIR__ . '/../includes/services.php';
+echo Services(
+    'Recommended',
+    'Lorem ipsum dolor sit amet consectetur. Eu quis enim tempor et proin neque.' );
+    ?>
 
-   <?php
-  include 'includes/subscribe.php'; ?>
+    <?php
+    require_once __DIR__ . '/../includes/testimonials.php';
+    echo Testimonials(
+        'Neque porro quisquam est qui dolum',
+        'Sign up for our newsletter to stay up-to-date on the latest promotions, discounts, and new features releases..' );
+        ?>
+
+        <?php
+        require_once __DIR__ . '/../includes/subscribe.php';
+        echo Subscribe(
+            'Subscribe to newsletter',
+            '"It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem of distribution it look like readable English."' );
+            ?>
